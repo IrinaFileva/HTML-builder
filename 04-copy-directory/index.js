bc7301da074051df7af9cadb51a1b7e622b45e7b
Irina Fileva <irinkaskai@gmail.com>
2024-01-20 14:10:30 +0500

    fix: error in solving a task  04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,21 +1,22 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
 const pathFile = path.join(__dirname, 'files');
 const pathFileCopy = path.join(__dirname, 'files-copy');
 
-function copyDir() {
-  fs.promises.mkdir(pathFileCopy, { recursive: true });
-  fs.readdir(pathFile, { withFileTypes: true }, (error, files) => {
-    if (error) console.log(error);
-    else {
-      files.forEach((file) => {
-        const fileName = file.name;
-        const startingFile = path.join(pathFile, fileName);
-        const finishedFile = path.join(pathFileCopy, fileName);
-        fs.promises.copyFile(startingFile, path.join(finishedFile));
-      });
+async function copyDir() {
+  try {
+    await fs.rm(pathFileCopy, { recursive: true, force: true });
+    await fs.mkdir(pathFileCopy, { recursive: true });
+    const files = await fs.readdir(pathFile, { withFileTypes: true });
+    for (let file of files) {
+      const fileName = file.name;
+      const startingFile = path.join(pathFile, fileName);
+      const finishedFile = path.join(pathFileCopy, fileName);
+      fs.copyFile(startingFile, path.join(finishedFile));
     }
-  });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 copyDir();
